9c5b6d828519e807afaf526ac2e6455b7558ef01
vin <vin@cybridge.jp>
2022-03-14 21:18:12 +0700

    update axios

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
-console.log('hello from main.js')
+import postApi from './api/postApi'
+
+async function main() {
+    try {
+        const queryParams = {
+            _page: 1,
+            _limit: 5,
+        }
+        const response = await postApi.getAll(queryParams)
+        console.log(response)
+    } catch (error) {}
+    await postApi.update({
+        id: 'sktwi1cgkkuif36dk',
+        title: 'Vin-cba-2022',
+    })
+}
+
+main()
